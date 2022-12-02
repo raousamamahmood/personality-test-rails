54b7c3cc81aad92416a43815ce679d49c7e0da3f
raousamamahmood <usama.mahmood@devsinc.com>
2022-12-02 14:27:14 +0500

    implemented using hash for answers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,29 +2,35 @@
 
 # QuestionsController
 class QuestionsController < ApplicationController
-  @@introvert = 0
-  @@extrovert = 0
+  @@answers = {}
 
   def start
+    @@answers.clear
     redirect_to question_path(Question.first)
   end
 
   def show
-    @question_no = @@introvert + @@extrovert + 1
+    @question_no = @@answers.length + 1
     @question = Question.find(params[:id])
   end
 
   def submit
-    params[:selected_option] == 'extrovert' ? @@extrovert += 1 : @@introvert += 1
-    next_question = Question.where('id > ?', params[:question_id]).first
-    if next_question
-      redirect_to question_path(next_question)
+    @@answers[params[:question_id]] = params[:selected_option]
+    new_question
+    if @next_question
+      redirect_to question_path(@next_question)
     else
       redirect_to :result
     end
   end
 
   def result
-    @result = @@introvert > @@extrovert ? 'Introvert' : 'Extrovert'
+    @result = @@answers.values.max_by { |value| @@answers.values.count(value) }
+  end
+
+  private
+
+  def new_question
+    @next_question = Question.where.not(id: @@answers.keys).first
   end
 end
