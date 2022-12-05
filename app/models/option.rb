7bd6c22445b59ba2986f75b00280b7d8572c3c3d
raousamamahmood <usama.mahmood@devsinc.com>
2022-12-05 17:17:33 +0500

    Backend validations

--- app/models/option.rb
+++ app/models/option.rb
@@ -2,6 +2,8 @@
 
 # OptionModel
 class Option < ApplicationRecord
+  validates :title, :option_type, presence: true
+
   belongs_to :question
 
   enum option_type: { introvert: 0, extrovert: 1 }
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,7 @@
 
 # QuestionsModel
 class Question < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+
   has_many :options, dependent: :destroy
 end
