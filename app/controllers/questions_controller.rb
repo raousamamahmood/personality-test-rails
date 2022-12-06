535bf6e50bc68c04fd0de5772f261c7b66ff91b1
raousamamahmood <usama.mahmood@devsinc.com>
2022-12-06 23:59:38 +0500

    syntax issue resolved

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,9 +5,4 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
-
-  def show
-    @question_no = @@answers.length + 1
-    @question = Question.find(params[:id])
-  end
 end
